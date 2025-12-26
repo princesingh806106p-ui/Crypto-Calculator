3f50f3ddba9e9013eec50b5f82482dc7b7385d11
account <account@example.com>
2025-12-27 02:38:21 +0530

    is Website ka URL bana do
    is Website ka URL bana do

---
+++ html/control.js
@@ -0,0 +1,7 @@
+if(5>3)
+{console,log("5>3");
+
+}
+else
+{console,log("5<3")}
+
---
+++ html/operator.js
@@ -0,0 +1,16 @@
+// let a=8;b=10;
+// let c=a<b &&b>a, d=a>b||b>a;
+// let e=!(a<b);
+// document.write(">"+(a>b)+"<br>");
+// document.write(">="+(a>=b)+"<br>");
+// document.write("<="+(a<=b)+"<br>");
+// document.write("==="+(a===b)+"<br>");
+// document.write("=="+(a==b)+"<br>");
+// document.write("!="+(a!=b)+"<br>");
+// document.write("!=="+(a!==b)+"<br>")
+// document.write("c="+c+"<br>");
+// document.write("d="+d+"<br>");
+// document.write("e="+e+"<br>");
+let x=4;y=10;z=9;
+document.write("largest_number"+(x>y>z)+"<br>");
+document.write("largest_number"+(x<y)+"<br>");
